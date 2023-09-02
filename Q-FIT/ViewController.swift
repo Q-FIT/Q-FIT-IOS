323440cbfb75558db4167a9e1ec9a95ad9ae684a
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-09-02 21:46:09 +0900

    Initial Commit

---
+++ Q-FIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Q-FIT
+//
+//  Created by 이서연 on 2023/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Q-FITUITests/Q_FITUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Q_FITUITestsLaunchTests.swift
+//  Q-FITUITests
+//
+//  Created by 이서연 on 2023/09/02.
+//
+
+import XCTest
+
+final class Q_FITUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
